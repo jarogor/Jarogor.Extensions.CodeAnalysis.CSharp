99e7a7ea6e869feeebb657fe4a7adf035d3b0a7c
Egor Chernyshov <jarogor@ya.ru>
2024-07-31 18:34:59 +0300

    документирование

--- Jarogor.Extensions.CodeAnalysis.CSharp/AttributeInfo.cs
+++ Jarogor.Extensions.CodeAnalysis.CSharp/AttributeInfo.cs
@@ -1,3 +1,9 @@
 ﻿namespace Jarogor.Extensions.CodeAnalysis.CSharp;
 
+/// <summary>
+/// Информация об атрибуте.
+/// </summary>
+/// <param name="Position">Позиция параметра</param>
+/// <param name="Name">Название параметра</param>
+/// <param name="Value">Значение параметра</param>
 public record struct AttributeInfo(int Position, string Name, string Value);

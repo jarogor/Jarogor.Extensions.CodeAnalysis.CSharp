c820a21c1101631530efcea3da1c1a12d2bc5f8e
Egor Chernyshov <jarogor@ya.ru>
2024-07-31 17:48:33 +0300

    форматирование, рефакторинг

--- Jarogor.Extensions.CodeAnalysis.CSharp.Test/MethodAttributesExtensionsTests.cs
+++ Jarogor.Extensions.CodeAnalysis.CSharp.Test/MethodAttributesExtensionsTests.cs
@@ -3,8 +3,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Jarogor.Extensions.CodeAnalysis.CSharp.Test;
 
-public class MethodAttributesExtensionsTests
-{
+public class MethodAttributesExtensionsTests {
     private const string CSharpCode
         = """
           using System;
@@ -16,7 +15,7 @@ public class MethodAttributesExtensionsTests
           {
               public class Program
               {
-                  static void Main(string[] args)
+                  public void Main(string[] args)
                   {
                       Console.WriteLine("Hello, World!");
                   }
@@ -24,49 +23,53 @@ public class MethodAttributesExtensionsTests
                   [Qwerty("1")]
                   [ Qwerty("2",Foo = "2")]
                   [QwertyAttribute  ("3", Foo = "3", Bar = "3")]
-                  [ Qwerty(name: "4", Bar = "4", Foo="4" ) ]
-                  [Qwerty  ( nameof( HelloWorld . Program . Do ), Bar = "5")]
                   public void Do()
+                  {
+                  }
+              }
+          }
+
+          namespace HelloWorld2
+          {
+              public class Program
+              {
+                  [ Qwerty(name: "4", Bar = "4", Foo="4" ) ]
+                  [Qwerty  ( nameof( HelloWorld2 . Program . Do2 ), Bar = "5")]
+                  public void Do2()
                   {
                   }
               }
           }
           """;
 
-    private static readonly List<List<AttributeInfo>> Expected = new()
-    {
-        new List<AttributeInfo> { new(0, string.Empty, "1") },
-        new List<AttributeInfo> { new(0, string.Empty, "2"), new(1, "Foo", "2") },
-        new List<AttributeInfo> { new(0, string.Empty, "3"), new(1, "Foo", "3"), new(2, "Bar", "3") },
-        new List<AttributeInfo> { new(0, "name", "4"), new(1, "Bar", "4"), new(2, "Foo", "4") },
-        new List<AttributeInfo> { new(0, string.Empty, "Do"), new(1, "Bar", "5") },
-    };
+    private static readonly List<List<AttributeInfo>> Expected = [
+        [new(0, string.Empty, "1")],
+        [new(0, string.Empty, "2"), new(1, "Foo", "2")],
+        [new(0, string.Empty, "3"), new(1, "Foo", "3"), new(2, "Bar", "3")],
+        [new(0, "name", "4"), new(1, "Bar", "4"), new(2, "Foo", "4")],
+        [new(0, string.Empty, "Do2"), new(1, "Bar", "5")],
+    ];
 
     private CompilationUnitSyntax? _root;
 
     [SetUp]
     public void SetUp()
-    {
-        _root = CSharpSyntaxTree.ParseText(CSharpCode).GetCompilationUnitRoot();
-    }
+        => _root = CSharpSyntaxTree.ParseText(CSharpCode).GetCompilationUnitRoot();
 
     [TestCase(" Qwerty ")]
     [TestCase(" QwertyAttribute ")]
-    public void TestFindAttributeValues(string attrName)
-    {
+    public void TestFindAttributeValues(string attrName) {
         var attributes = _root!.FindMethodsAttributes(attrName).ToList();
-        Assert.AreEqual(Expected.Count, attributes.Count);
+        Assert.That(attributes, Has.Count.EqualTo(Expected.Count));
 
-        for (var a = 0; a < attributes.Count; a++)
-        {
-            Assert.NotNull(attributes[a]);
+        for (var a = 0; a < attributes.Count; a++) {
+            Assert.That(attributes[a], Is.Not.Null);
 
             var args = attributes[a]!.ToList();
-            Assert.AreEqual(Expected[a].Count, args.Count);
+            Assert.That(args, Has.Count.EqualTo(Expected[a].Count));
 
-            for (var b = 0; b < args.Count; b++)
-            {
-                Assert.AreEqual(Expected[a][b], args[b]);
+            for (var b = 0; b < args.Count; b++) {
+                Assert.That(args[b], Is.EqualTo(Expected[a][b]));
             }
         }
     }
--- Jarogor.Extensions.CodeAnalysis.CSharp/MethodAttributesExtensions.cs
+++ Jarogor.Extensions.CodeAnalysis.CSharp/MethodAttributesExtensions.cs
@@ -3,59 +3,54 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Jarogor.Extensions.CodeAnalysis.CSharp;
 
-public static class MethodAttributesExtensions
-{
+/// <summary>
+/// Метод расширения поиска атрибутов методов.
+/// </summary>
+public static class MethodAttributesExtensions {
     private const string AttributeSuffix = "Attribute";
 
+    /// <summary>
+    /// Поиск атрибутов методов по имени
+    /// </summary>
+    /// <param name="root">Объект CompilationUnitSyntax</param>
+    /// <param name="name">Имя атрибута</param>
+    /// <returns>Двухуровневая коллекция найденных атрибутов. Внутри каждого элемента атрибута коллекция AttributeInfo — информация о параметрах.</returns>
     public static IEnumerable<IEnumerable<AttributeInfo>?> FindMethodsAttributes(this CompilationUnitSyntax root, string name)
-    {
-        return root
+        => root
             .GetClassDeclarationSyntaxList()
             .GetMethodDeclarationSyntaxList()
             .GetAttributeSyntaxList(name.Trim())
             .GetAttributeInfoList();
-    }
 
     private static IEnumerable<ClassDeclarationSyntax> GetClassDeclarationSyntaxList(this SyntaxNode self)
-    {
-        return self
+        => self
             .DescendantNodes()
             .Where(it => it is ClassDeclarationSyntax)
             .Cast<ClassDeclarationSyntax>();
-    }
 
     private static IEnumerable<MethodDeclarationSyntax> GetMethodDeclarationSyntaxList(this IEnumerable<ClassDeclarationSyntax> self)
-    {
-        return self
+        => self
             .SelectMany(it => it.ChildNodes())
             .Where(m => m is MethodDeclarationSyntax)
             .Cast<MethodDeclarationSyntax>()
             .Where(IsPublic);
-    }
 
     private static bool IsPublic(this MethodDeclarationSyntax it)
         => it.Modifiers.Any(t => t.ValueText == "public");
 
     private static IEnumerable<AttributeSyntax> GetAttributeSyntaxList(this IEnumerable<MethodDeclarationSyntax> self, string name)
-    {
-        return self
+        => self
             .SelectMany(it => it.DescendantNodes().OfType<AttributeListSyntax>())
             .SelectMany(it => it.Attributes)
             .Where(a => a.IsAttribute(name));
-    }
 
-    private static bool IsAttribute(this AttributeSyntax self, string name)
-    {
+    private static bool IsAttribute(this AttributeSyntax self, string name) {
         var attributeNames = new string[2];
         attributeNames[0] = name;
 
-        if (name.EndsWith(AttributeSuffix))
-        {
-            var position = name.IndexOf(AttributeSuffix, StringComparison.Ordinal);
-            attributeNames[1] = name[..position];
-        }
-        else
-        {
+        if (name.EndsWith(AttributeSuffix)) {
+            attributeNames[1] = name.Substring(0, name.Length - AttributeSuffix.Length);
+        } else {
             attributeNames[1] = $"{name}{AttributeSuffix}";
         }
 
@@ -63,26 +58,21 @@ public static class MethodAttributesExtensions
     }
 
     private static IEnumerable<IEnumerable<AttributeInfo>?> GetAttributeInfoList(this IEnumerable<AttributeSyntax> self)
-    {
-        return self
+        => self
             .Select(it => it.ArgumentList)
             .Select(it
                 => (it?.Arguments.ToList() ?? new List<AttributeArgumentSyntax>())
                 .Select((argument, index) => new AttributeInfo(index, argument.GetName(), argument.Expression.GetValue()))
             );
-    }
 
     private static string GetName(this AttributeArgumentSyntax self)
         => self.NameColon?.Name.Identifier.ValueText ?? self.NameEquals?.Name.Identifier.ValueText ?? string.Empty;
 
     private static string GetValue(this ExpressionSyntax self)
-    {
-        return self switch
-        {
+        => self switch {
             // EXAMPLE: nameof(qwerty.foo.bar) -> qwerty.foo.bar -> last: bar
             InvocationExpressionSyntax invocation => invocation.DescendantNodes().Last().GetLastToken().ValueText,
             LiteralExpressionSyntax literal => literal.Token.ValueText,
             _ => string.Empty,
         };
-    }
 }
